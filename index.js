f52d3a33b5d944c3cb56556c4346995b9b69881e
Qian <ee.xuqian@gmail.com>
2017-04-27 19:11:26 -0700

    change style to match heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000));
 
 // set up a default router
 var router = express.Router();
@@ -68,5 +69,8 @@ router.route('/')
 
 app.use("/", router);
 
-app.listen(5000);
-console.log("starting express server on port 9100...");
+//app.listen(5000);
+//console.log("starting express server on port 9100...");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
